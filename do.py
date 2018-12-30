5ed258af3589d07081d4b578edd9d0b38470288d
Yanghao Hua <huayanghao@gmail.com>
2018-12-30 16:17:46 +0000

    add access script

---
+++ do.py
@@ -0,0 +1,21 @@
+import time
+import socket
+from urllib.request import urlopen
+
+hostname = socket.gethostname()
+
+delta = 30
+
+last = time.time()
+while True:
+    now = time.time()
+    if (now - last) > delta:
+        try:
+            print("now ...")
+            content = urlopen("http://192.168.0.18:8080/%s" % hostname).read()
+            print(b"content: %s" % content)
+        except:
+            print("access to 192.168.0.18:8080 failed")
+        last = now
+    else:
+        time.sleep(5)
