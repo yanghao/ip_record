07ac64de0ebed72d162cd578ddc4e55c0fd30626
Hua Yanghao <yanghao.hua@intel.com>
2019-01-12 13:34:47 +0100

    minor update

--- do.py
+++ do.py
@@ -2,6 +2,7 @@ import time
 import socket
 from urllib.request import urlopen
 
+server = ["192.168.0.10", 8080]
 hostname = socket.gethostname()
 
 delta = 30
@@ -12,10 +13,12 @@ while True:
     if (now - last) > delta:
         try:
             print("now ...")
-            content = urlopen("http://192.168.0.18:8080/%s" % hostname).read()
+            server_and_name = server + [hostname]
+            print(server_and_name)
+            content = urlopen("http://%s:%d/%s" % tuple(server_and_name)).read()
             print(b"content: %s" % content)
         except:
-            print("access to 192.168.0.18:8080 failed")
+            print("access to %s:%d failed" % server)
         last = now
     else:
         time.sleep(5)
