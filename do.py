f780ef30ff1d2bbc0f559f368c872181389a63b0
Hua Yanghao <yanghao.hua@intel.com>
2019-01-17 22:15:02 +0100

    set a timeout on request

--- do.py
+++ do.py
@@ -2,6 +2,8 @@ import time
 import socket
 import logging
 from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+from socket import timeout
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger("main")
@@ -13,6 +15,7 @@ hostname = socket.gethostname()
 delta = 30
 
 last = 0
+n = 0
 while True:
     now = time.time()
     if (now - last) > delta:
@@ -20,8 +23,16 @@ while True:
             log.info("now ...")
             server_and_name = server + [hostname]
             log.info(server_and_name)
-            content = urlopen("http://%s:%d/%s" % tuple(server_and_name)).read()
-            log.info(b"content: %s" % content)
+            try:
+                content = urlopen("http://%s:%d/%s" % tuple(server_and_name), timeout=10).read()
+                log.info(b"content: %s - %d" % (content, n))
+                n += 1
+            except (HTTPError, URLError) as error:
+                log.error('Data not retrieved because: %s', error)
+            except timeout:
+                log.error('socket timed out')
+            else:
+                log.info('Access successful.')
         except:
             log.info("access to %s:%d failed" % server)
         last = now
